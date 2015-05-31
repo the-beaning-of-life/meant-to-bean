7d67650894720e5ac47840863802db5e04b30c18
Lee Richardson <lee.richardson4@gmail.com>
2015-05-30 20:44:20 -0400

    edited menu

--- menu.php
+++ menu.php
@@ -2,27 +2,80 @@
 /*
 Template Name: Menu Page
 */
- ?>
+?>
 
 <?php get_header();  ?>
 
 <div class="main">
-<div class="menuHeader">
-	<h1>Menu</h1>
-</div>
-  <div class="container">
+	<div class="menuHeader">
+		<h1>Menu</h1>
+	</div>
+	<div class="container">
 
+		
+		<div class="menuTextContainer">
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur incidunt iste veniam beatae, blanditiis ipsam deleniti eius quaerat repudiandae similique placeat quia optio velit eaque quos amet deserunt reiciendis at error recusandae, et soluta nulla! Consectetur, velit repellendus? Voluptate sequi quibusdam ut suscipit eius consequuntur inventore, cum consequatur a! Ad accusantium sit fugit necessitatibus magnam, ratione repudiandae consectetur et quasi? Aspernatur illo necessitatibus quis, maiores in excepturi accusamus ex. Beatae iusto qui debitis hic sequi.</p>
+		</div>
+		
+		<div class="foodColumnContainer clearfix">
+			<div class="foodColumn">
+				<img src="https://download.unsplash.com/reserve/oMRKkMc4RSq7N91OZl0O_IMG_8309.jpg" alt="">
+				<h2>Salads</h2>
 
-     <!--  <?php // Start the loop ?>
-      <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<h4>Halloumi & honey</h4>
+				<p>Romaine lettuce, bulgur, sundried tomatoes, walnuts, peppers, red onion, peas and honey</p>
+				<h4>Chicken & Bacon</h4>
+				<p>Romaine lettuce, chicken, bacon, parmesan, croutons, tomato and red onion. Served with Caesar dressing.</p>
+				<h4>Bulgur, nuts and vegetables</h4>
+				<p>Romaine lettuce, bulgur, walnuts, cashew nuts, sundried tomato, pickled red onion, olives, carrots, peppers and peas.</p>	
+			</div>
+
+			<div class="foodColumn">
+				<img src="https://download.unsplash.com/reserve/oMRKkMc4RSq7N91OZl0O_IMG_8309.jpg" alt="">
+
+				<h2>Cold sandwiches</h2>
+				<h4>Chicken Cashew with hot pineapple salsa</h4>
+				<p>Flatbread, Grilled Swedish chicken breast, cream cheese, hot pineapple salsa, cashew nuts and cilantro.</p>
+				<h4>Mozzarella & Tomato</h4>
+				<p>Bread with seasonally available tomatoes, mozzarella, pickled red onion, lettuce and our own homegrown basil and canola oil.</p>
+				<h4>Shrimp & mayo</h4>
+				<p>Bread with seasonal shrimp and our homemade mayonnaise, flavoured with lemon and lots of our homegrown dill.</p>	
+			</div>
+
+			<div class="foodColumn">
+				<img src="https://download.unsplash.com/reserve/oMRKkMc4RSq7N91OZl0O_IMG_8309.jpg" alt="">
+				<h2>Pressed sandwiches</h2>
+				<h4>Cheese & ham with tomato</h4>
+				<p>Our fresh-baked panini with smoked ham, seasonally available tomatoes, cheese and dijon mayonnaise.</p>
+				<h4>Halloumi, thyme and honey </h4>
+				<p>Our fresh-baked panini with halloumi cheese, cream cheese, honey and fresh thyme.</p>
+				<h4>Chicken, Bacon and tomato</h4>
+				<p>Our fresh-baked panini with grilled chicken breast, tomato, cheese, bacon and dijon mayonnaise.</p>	
+			</div>
+		</div>	
+
 	
-  	<?php the_title(); ?>
-       <?php the_content(); ?> 
+	</div>
+
+		<div class="menuBannerImage">
+			<h2>Pastries and coffee</h2>
+		</div>
+
+		<div class="pastryColumn">
+			<p>Cinammon bun</p>
+			<p>Cardamom Bun</p>
+			<p>Croissant</p>
+			<p>Muffins</p>
+		</div>
 
 
-      <?php endwhile; // end the loop?>
-    </div> <!-- /,content -->
-  </div> <!-- /.container -->
+
+	
+
+	
+		
+
+	</div> <!-- /.container -->
 </div> <!-- /.main -->
 
 <?php get_footer(); ?>
